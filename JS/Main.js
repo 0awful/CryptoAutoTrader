bf7688b524d13d913037808914e6f5f51ca11808
izaac <im.izaac.seo@gmail.com>
2017-12-29 18:07:32 -0800

    Looped main.js, added call to cleanup function
    Now it loops based upon a given input in miliseconds

--- JS/Main.js
+++ JS/Main.js
@@ -10,102 +10,119 @@ const orders = require('./CreateOrders');
 const round = require('./Round');
 const decimals = require('./FindDecimals');
 const filters = require('./OrderFilters');
+const cleanup = require('./Cleanup');
 
-let walletBalances;
-let walletPrices;
-let exchangeInfo;
-let normalizedData;
-let walletAverage;
-let split;
+function orderLoop(timeBetweenLoops) {
+  let walletBalances;
+  let walletPrices;
+  let exchangeInfo;
+  let normalizedData;
+  let walletAverage;
+  let split;
 
-let walletPromise = balances.fetch();
-let pricesPromise = prices.fetch();
-let exchangePromise = exchange.fetch();
+  let walletPromise = balances.fetch();
+  let pricesPromise = prices.fetch();
+  let exchangePromise = exchange.fetch();
 
-let fetchArray = [walletPromise, pricesPromise, exchangePromise];
+  let fetchArray = [walletPromise, pricesPromise, exchangePromise];
 
-Promise.all(fetchArray)
-  .then(function(data) {
-    walletBalances = data[0];
-    walletPrices = data[1];
-    exchangeInfo = data[2];
+  Promise.all(fetchArray)
+    .then(function(data) {
+      walletBalances = data[0];
+      walletPrices = data[1];
+      exchangeInfo = data[2];
 
-    console.log('Normalizing');
-    normalizedData = normalize.normalize(
-      walletBalances,
-      walletPrices,
-      exchangeInfo
-    );
-    console.log('Averaging');
-
-    walletAverage = average.average(normalizedData);
-    console.log('Splitting coins');
-    split = splitCoins.split(normalizedData, walletAverage);
-
-    return new Promise(function(resolve, reject) {
-      let promises = split.high.map(
-        item =>
-          new Promise(function(resolve, reject) {
-            setTimeout(function() {
-              let quantity = item.coins.toFixed(
-                Math.abs(decimals.find(item.info.LOT_SIZE.stepSize))
-              );
-
-              if (
-                filters.lotSize(item.info.LOT_SIZE, quantity) &&
-                filters.minNotional(
-                  item.info.MIN_NOTIONAL,
-                  quantity,
-                  item.price
-                )
-              ) {
-                orders.sell(item.name, quantity).then(function() {
-                  resolve();
-                });
-              } else {
-                console.log(item.name, 'failed');
-                resolve();
-              }
-            }, 1000 + 200 * split.high.indexOf(item));
-          })
+      console.log('Normalizing');
+      normalizedData = normalize.normalize(
+        walletBalances,
+        walletPrices,
+        exchangeInfo
       );
-      Promise.all(promises).then(function() {
-        console.log('All sell orders have resolved');
-        resolve();
-      });
-    });
-  })
-  .then(function() {
-    return new Promise(function(resolve, reject) {
-      let promises = split.low.map(
-        item =>
-          new Promise(function(resolve, reject) {
-            setTimeout(function() {
-              let quantity = item.coins.toFixed(
-                Math.abs(decimals.find(item.info.LOT_SIZE.stepSize))
-              );
+      console.log('Averaging');
 
-              if (
-                filters.lotSize(item.info.LOT_SIZE, quantity) &&
-                filters.minNotional(
-                  item.info.MIN_NOTIONAL,
-                  quantity,
-                  item.price
-                )
-              ) {
-                orders.buy(item.name, quantity).then(function() {
+      walletAverage = average.average(normalizedData);
+      console.log('Splitting coins');
+      split = splitCoins.split(normalizedData, walletAverage);
+
+      return new Promise(function(resolve, reject) {
+        let promises = split.high.map(
+          item =>
+            new Promise(function(resolve, reject) {
+              setTimeout(function() {
+                let quantity = item.coins.toFixed(
+                  Math.abs(decimals.find(item.info.LOT_SIZE.stepSize))
+                );
+
+                if (
+                  filters.lotSize(item.info.LOT_SIZE, quantity) &&
+                  filters.minNotional(
+                    item.info.MIN_NOTIONAL,
+                    quantity,
+                    item.price
+                  )
+                ) {
+                  orders.sell(item.name, quantity).then(function() {
+                    resolve();
+                  });
+                } else {
+                  console.log(item.name, 'failed');
                   resolve();
-                });
-              } else {
-                console.log(item.name, 'failed');
-                resolve();
-              }
-            }, 1000 + 200 * split.low.indexOf(item));
-          })
-      );
-      Promise.all(promises).then(function() {
-        console.log('All buy orders have resolved');
-        resolve();
+                }
+              }, 1000 + 200 * split.high.indexOf(item));
+            })
+        );
+        Promise.all(promises).then(function() {
+          console.log('All sell orders have resolved');
+          resolve();
+        });
       });
+    })
+    .then(function() {
+      return new Promise(function(resolve, reject) {
+        let promises = split.low.map(
+          item =>
+            new Promise(function(resolve, reject) {
+              setTimeout(function() {
+                let quantity = item.coins.toFixed(
+                  Math.abs(decimals.find(item.info.LOT_SIZE.stepSize))
+                );
+
+                if (
+                  filters.lotSize(item.info.LOT_SIZE, quantity) &&
+                  filters.minNotional(
+                    item.info.MIN_NOTIONAL,
+                    quantity,
+                    item.price
+                  )
+                ) {
+                  orders.buy(item.name, quantity).then(function() {
+                    resolve();
+                  });
+                } else {
+                  console.log(item.name, 'failed');
+                  resolve();
+                }
+              }, 1000 + 200 * split.low.indexOf(item));
+            })
+        );
+        Promise.all(promises).then(function() {
+          console.log('All buy orders have resolved');
+          resolve();
+        });
+      });
+    })
+    .then(function() {
+      cleanup.cleanup();
+
+      // this represents running every 30 minutes
+      let waitTime = timeBetweenLoops;
+
+      console.log('restarting in', waitTime / 60000, 'minutes');
+      setTimeout(function() {
+        console.log('Restarting cycle');
+        orderLoop();
+      }, waitTime);
     });
-  });
+}
+
+orderLoop(1800000);
