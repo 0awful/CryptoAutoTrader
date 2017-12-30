6dc8f6c2533e2fadd28b775bcc17da729a2c30e0
izaac <im.izaac.seo@gmail.com>
2017-12-29 18:04:09 -0800

    Created placeholder cleanup function
    This will end up doing the things that we need to have cleaned up
    between rounds. As of yet I have not found a need for this, but it
    should be a thing eventually.

---
+++ JS/Cleanup.js
@@ -0,0 +1,9 @@
+function cleanup() {
+  console.log('WARNING: IMPLEMENT CLEANUP');
+  console.log(
+    'Cleanup called, but cannot complete because it has not been implemented'
+  );
+}
+
+module.exports.cleanup = cleanup;
+
