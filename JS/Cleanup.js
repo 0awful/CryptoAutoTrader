ac2daf7555860950ea6d1c43a345ca4c3537caa2
izaac <im.izaac.seo@gmail.com>
2017-12-30 16:46:26 -0800

    Removed Cleanup.js and all calls to it It has never been relavent over two days of use. Market buys execute immediately. The switch to it removed the nessecessity of this step.

--- JS/Cleanup.js
+++
@@ -1,9 +0,0 @@
-function cleanup() {
-  console.log('WARNING: IMPLEMENT CLEANUP');
-  console.log(
-    'Cleanup called, but cannot complete because it has not been implemented'
-  );
-}
-
-module.exports.cleanup = cleanup;
-
