c35ae5fd417969a470bf9ae7dd515d82e392f819
imizaac <im.izaac.seo@gmail.com>
2018-01-09 17:04:37 -0800

    made server

--- JS/Server.js
+++ JS/Server.js
@@ -1,7 +1,34 @@
 const algo = require('./Main.js');
+var http = require('http'),
+  httpserv = http.createServer(handleHTTP),
+  port = 8006,
+  host = 'localhost',
+  fs = require('fs'),
+  index = fs.readFileSync('../index.html');
+ENV = 'test';
+function handleHTTP(req, res) {
+  console.log('request starting ');
+  if (req.method === 'GET') {
+    console.log('found get request', req.url);
+    if (req.url == '/index.html' || req.url == '/' || req.url == '/index') {
+      res.writeHead(200, { 'Content-type': 'text/html' });
+      res.end(index);
+    } else {
+      res.writeHead(403);
+      res.end('403');
+    }
+  } else {
+    res.writeHead(403);
+    res.end('403');
+  }
+}
 
 // in miliseconds
 let timeout = 900000;
+console.log('server starting');
+httpserv.listen(port, host);
 
-algo.main();
-setInterval(algo.main, timeout);
+if (ENV !== 'test') {
+  algo.main();
+  setInterval(algo.main, timeout);
+}
