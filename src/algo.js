3a48a8d7a9ee4175826c02476ab8d27a832867fe
Izaac Wilkowski <im.izaac.seo@gmail.com>
2021-05-09 10:13:14 -0700

    CLEAN UP

--- src/algo.js
+++ src/algo.js
@@ -4,22 +4,8 @@ const { orderTypes } = require('./orderDTO');
 const { createOrders } = require('./createOrders');
 const { isSet } = require('./utils');
 const { processOrders } = require('./processOrders');
-const fs = require('fs');
-const exchange = require('./FetchExchangeInfo');
-
-const normalize = require('./Normalize');
-const splitCoins = require('./SplitCoins');
-const decimals = require('./FindDecimals');
-const filters = require('./OrderFilters');
 
 const algo = async () => {
-  // let walletBalances;
-  // let walletPrices;
-  // let exchangeInfo;
-  // let normalizedData;
-  // let walletAverage;
-  // let split;
-
   let { free } = await balances.fetch();
 
   const price = await prices.fetch();
@@ -55,87 +41,6 @@ const algo = async () => {
 
   console.log(await processOrders(sellOrders));
   console.log(await processOrders(buyOrders));
-  // await processOrders(sellOrders);
-  // fs.writeFileSync('tooHigh.json', JSON.stringify(tooHigh));
-
-
-  // let price = await prices.fetch();
-  // let exchangePromise = exchange.fetch();
-
-  // Promise.all(fetchArray)
-  //   .then(function(data) {
-  //     walletBalances = data[0];
-  //     walletPrices = data[1];
-  //     exchangeInfo = data[2];
-
-  //     console.log('Normalizing');
-  //     normalizedData = normalize.normalize(
-  //       walletBalances,
-  //       walletPrices,
-  //       exchangeInfo
-  //     );
-  //     console.log('Averaging');
-
-  //     walletAverage = average.average(normalizedData);
-  //     console.log('Splitting coins');
-  //     split = splitCoins.split(normalizedData, walletAverage);
-
-  //     return new Promise(function(resolve, reject) {
-  //       let promises = split.high.map(
-  //         item =>
-  //           new Promise(function(resolve, reject) {
-  //             setTimeout(function() {
-  //               let quantity = item.coins.toFixed(
-  //                 Math.abs(decimals.find(item.info.LOT_SIZE.stepSize))
-  //               );
-
-  //               if (filters.lotSize(item.info.LOT_SIZE, quantity)) {
-  //                 orders.sell(item.name, quantity).then(function() {
-  //                   resolve();
-  //                 });
-  //               } else {
-  //                 console.log(item.name, 'failed');
-  //                 resolve();
-  //               }
-  //             }, 1000 + 200 * split.high.indexOf(item));
-  //           })
-  //       );
-  //       Promise.all(promises).then(function() {
-  //         console.log('All sell orders have resolved');
-  //         resolve();
-  //       });
-  //     });
-  //   })
-  //   .then(function() {
-  //     return new Promise(function(resolve, reject) {
-  //       let promises = split.low.map(
-  //         item =>
-  //           new Promise(function(resolve, reject) {
-  //             setTimeout(function() {
-  //               let quantity = item.coins.toFixed(
-  //                 Math.abs(decimals.find(item.info.LOT_SIZE.stepSize))
-  //               );
-
-  //               if (filters.lotSize(item.info.LOT_SIZE, quantity)) {
-  //                 orders.buy(item.name, quantity).then(function() {
-  //                   resolve();
-  //                 });
-  //               } else {
-  //                 console.log(item.name, 'failed');
-  //                 resolve();
-  //               }
-  //             }, 1000 + 200 * split.low.indexOf(item));
-  //           })
-  //       );
-  //       Promise.all(promises).then(function() {
-  //         console.log('All buy orders have resolved');
-  //         resolve();
-  //       });
-  //     });
-  //   })
-  //   .then(function() {
-  //     console.log('Algorithm complete');
-  //   });
 }
 
 module.exports.algo = algo;
