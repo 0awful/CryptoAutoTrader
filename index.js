6ef52b4fb994cde1b4e3a575556fb3d02c24ae03
Izaac Wilkowski <im.izaac.seo@gmail.com>
2021-05-09 19:30:41 -0700

    updated naming conventions

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const { main } = require('./src/main');
+const { API_KEY, API_SECRET, BASE_HTTP_URL } = require('./src/config');
 
-main();
+main({
+    apiKey: API_KEY,
+    secret: API_SECRET,
+    httpBase: BASE_HTTP_URL,
+    sandboxMode: false,
+    enableRateLimit: true,
+});
