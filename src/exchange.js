68a19fc2e73882bae95a90898dd2f9b870441d68
Izaac Wilkowski <im.izaac.seo@gmail.com>
2021-05-09 19:14:59 -0700

    rename

---
+++ src/exchange.js
@@ -0,0 +1,24 @@
+const ccxt = require('ccxt');
+
+let client;
+
+const getClient = () => {
+    if (!client) throw new Error('client requested before instantiation');
+    return client;
+};
+
+const initClient = ({apiKey, secret, httpBase, sandboxMode, enableRateLimit = true}) => {
+    if (client) throw new Error('double client instantiation error');
+    client = new ccxt.binanceus({
+        apiKey,
+        secret,
+        httpBase,
+        enableRateLimit,
+    });
+    client.setSandboxMode(sandboxMode);
+};
+
+module.exports = {
+    getClient,
+    initClient,
+};
