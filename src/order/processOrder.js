bb7a23915ca55d150bf9d2255e6a9cb2b0bb939b
Izaac Wilkowski <im.izaac.seo@gmail.com>
2021-05-12 19:17:35 -0700

    it was a bug all along

--- src/order/processOrder.js
+++ src/order/processOrder.js
@@ -1,15 +1,13 @@
 const { getClient } = require("../exchange");
-const { SELL, BUY } = require("./orderDTO");
+const { orderTypes } = require("./orderDTO");
 
 const processOrder = ({ orderType, symbol, amount }) => {
   const client = getClient();
 
-  if (orderType === SELL) {
+  if (orderType === orderTypes.SELL)
     return client.createMarketSellOrder(symbol, amount);
-  }
-  if (orderType === BUY) {
+  if (orderType === orderTypes.BUY)
     return client.createMarketBuyOrder(symbol, amount);
-  }
 };
 
 module.exports = {
