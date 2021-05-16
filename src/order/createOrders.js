50c3c00efb5c3e694094242b3417ac6f5ba98c63
Izaac Wilkowski <im.izaac.seo@gmail.com>
2021-05-15 19:58:49 -0700

    tolerance

--- src/order/createOrders.js
+++ src/order/createOrders.js
@@ -1,7 +1,9 @@
 const { createOrderDTO } = require("./orderDTO");
 
-const createOrders = (obj, average) => {
-  return Object.values(obj).map((values) => createOrderDTO(values, average));
+const createOrders = (obj, average, tolerance) => {
+  return Object.values(obj).map((values) =>
+    createOrderDTO(values, average, tolerance)
+  );
 };
 
 module.exports = {
