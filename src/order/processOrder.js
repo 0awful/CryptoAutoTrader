b93bcfc754c72499e2c68a22e44361e6aa53269c
izaac <im.izaac.seo@gmail.com>
2021-08-06 20:05:03 -0700

    implement error handling eventually

--- src/order/processOrder.js
+++ src/order/processOrder.js
@@ -4,10 +4,14 @@ const { orderTypes } = require("./orderDTO");
 const processOrder = ({ orderType, symbol, amount }) => {
   const client = getClient();
 
-  if (orderType === orderTypes.SELL)
-    return client.createMarketSellOrder(symbol, amount);
-  if (orderType === orderTypes.BUY)
-    return client.createMarketBuyOrder(symbol, amount);
+  try {
+    if (orderType === orderTypes.SELL)
+      return client.createMarketSellOrder(symbol, amount);
+    if (orderType === orderTypes.BUY)
+      return client.createMarketBuyOrder(symbol, amount);
+  } catch (e) {
+    console.error({e, orderType, symbol, amount})
+  }
 };
 
 module.exports = {
